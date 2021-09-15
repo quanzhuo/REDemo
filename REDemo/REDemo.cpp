d9e71ae4d52401e6c58a8944004f80166e94ffc4
Quan <zhuo.quan@mail.foxconn.com>
2021-09-15 23:42:57 +0800

    Init

---
+++ REDemo/REDemo.cpp
@@ -0,0 +1,306 @@
+// REDemo.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "REDemo.h"
+#include "RichEdit.h"
+#include "Richole.h"
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_REDEMO, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_REDEMO));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_REDEMO));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_REDEMO);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // Store instance handle in our global variable
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+BOOL InsertObject(HWND hRichEdit, LPCTSTR pszFileName)
+{
+    HRESULT hr;
+    LPRICHEDITOLE pRichEditOle;
+    SendMessage(hRichEdit, EM_GETOLEINTERFACE, 0, (LPARAM)&pRichEditOle);
+    if (pRichEditOle == NULL)
+    {
+        return FALSE;
+    }
+
+    LPLOCKBYTES pLockBytes = NULL;
+    hr = CreateILockBytesOnHGlobal(NULL, TRUE, &pLockBytes);
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+
+    LPSTORAGE pStorage;
+    hr = StgCreateDocfileOnILockBytes(pLockBytes,
+        STGM_SHARE_EXCLUSIVE | STGM_CREATE | STGM_READWRITE,
+        0, &pStorage);
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+
+    FORMATETC formatEtc;
+    formatEtc.cfFormat = 0;
+    formatEtc.ptd = NULL;
+    formatEtc.dwAspect = DVASPECT_CONTENT;
+    formatEtc.lindex = -1;
+    formatEtc.tymed = TYMED_NULL;
+    LPOLECLIENTSITE pClientSite;
+    hr = pRichEditOle->GetClientSite(&pClientSite);
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+
+    LPUNKNOWN pUnk;
+    CLSID clsid = CLSID_NULL;
+    hr = OleCreateFromFile(clsid, pszFileName, IID_IUnknown, OLERENDER_DRAW,
+        &formatEtc, pClientSite, pStorage, (void**)&pUnk);
+    pClientSite->Release();
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+
+    LPOLEOBJECT pObject;
+    hr = pUnk->QueryInterface(IID_IOleObject, (void**)&pObject);
+    pUnk->Release();
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+
+    OleSetContainedObject(pObject, TRUE);
+    REOBJECT reobject = { sizeof(REOBJECT) };
+    hr = pObject->GetUserClassID(&clsid);
+    if (FAILED(hr))
+    {
+        pObject->Release();
+        return FALSE;
+    }
+
+    reobject.clsid = clsid;
+    reobject.cp = REO_CP_SELECTION;
+    reobject.dvaspect = DVASPECT_CONTENT;
+    reobject.dwFlags = REO_RESIZABLE | REO_BELOWBASELINE;
+    reobject.dwUser = 0;
+    reobject.poleobj = pObject;
+    reobject.polesite = pClientSite;
+    reobject.pstg = pStorage;
+    SIZEL sizel = { 0 };
+    reobject.sizel = sizel;
+    SendMessage(hRichEdit, EM_SETSEL, 0, -1);
+    DWORD dwStart, dwEnd;
+    SendMessage(hRichEdit, EM_GETSEL, (WPARAM)&dwStart, (LPARAM)&dwEnd);
+    SendMessage(hRichEdit, EM_SETSEL, dwEnd + 1, dwEnd + 1);
+    SendMessage(hRichEdit, EM_REPLACESEL, TRUE, (WPARAM)L"\n");
+    hr = pRichEditOle->InsertObject(&reobject);
+    pObject->Release();
+    pRichEditOle->Release();
+    if (FAILED(hr))
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HWND hwndEdit, hwndBtnInsertFile, hwndBtnInsertImage;
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        LoadLibrary(TEXT("MSFTEDIT.dll"));
+        hwndEdit = CreateWindowEx(0, MSFTEDIT_CLASS, TEXT("Type here"),
+            ES_MULTILINE | WS_VISIBLE | WS_CHILD | WS_BORDER | WS_TABSTOP | WS_VSCROLL | WS_HSCROLL,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+            hWnd, (HMENU)1, hInst, NULL);
+        hwndBtnInsertFile = CreateWindowEx(0, TEXT("BUTTON"), TEXT("Insert File"),
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            hWnd, (HMENU)2, hInst, NULL);
+        hwndBtnInsertImage = CreateWindowEx(0, TEXT("BUTTON"), TEXT("Insert Image"),
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            hWnd, (HMENU)3, hInst, NULL);
+        break;
+    }
+    case WM_SIZE:
+    {
+        WORD cx = LOWORD(lParam), cy = HIWORD(lParam);
+        int btnHeight = 50, vmargin = 10, hmargin = 10, btnWidth = 200;
+        SetWindowPos(hwndEdit, HWND_TOP, 0, btnHeight+vmargin, cx, cy - btnHeight-vmargin, SWP_SHOWWINDOW);
+        SetWindowPos(hwndBtnInsertFile, HWND_TOP, 0, 0,  btnWidth, btnHeight, SWP_SHOWWINDOW);
+        SetWindowPos(hwndBtnInsertImage, HWND_TOP, btnWidth + hmargin, 0, btnWidth, btnHeight, SWP_SHOWWINDOW);
+        break;
+    }
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        case 2:
+            //MessageBoxW(hWnd, L"按钮被单击", L"警告", MB_ICONWARNING);
+            InsertObject(hwndEdit, L"C:\\Users\\zhuoq\\Desktop\\1.txt");
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: Add any drawing code that uses hdc here...
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
